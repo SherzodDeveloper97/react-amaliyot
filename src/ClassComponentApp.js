2e5d16def5b36bd6860778578bd1ef2d01efb3da
Sherzod Aliyev <shortfilmuz@gmail.com>
2023-08-23 01:24:54 +0500

    add ClassComponentApp

---
+++ src/ClassComponentApp.js
@@ -0,0 +1,62 @@
+import {Component} from "react";
+
+class ClassComponentApp extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            counter: 0,
+            age: "",
+        }
+    }
+
+    changeCounter(btn) {
+        const {counter} = this.state;
+        if(btn === "add"){
+            this.setState({
+                counter: counter + 1
+            })
+        }else if(btn === "delete"){
+            this.setState({
+                counter: counter - 1
+            })
+        }else if(btn === "reset"){
+            this.setState({
+                counter: 0,
+            })
+        }
+    }
+
+    changeHandler = (e) => {
+        console.log(e.target.value);
+        this.setState({
+            age: e.target.value,
+        })
+    }
+
+    render() {
+        // console.log(this.props);
+        const {name} = this.props;
+        const {counter} = this.state;
+        return (
+            <div>
+                <h1>{name}</h1>
+                <div className="counter">
+                    <button onClick={() => this.changeCounter("add")}>Add + 1</button>
+                    <h2>{`Counter: ${counter}`}</h2>
+                    <button onClick={() => this.changeCounter("delete")}>Delete - 1</button>
+                    <br /><br />
+                    <button onClick={() => this.changeCounter("reset")}>Reset</button>
+                </div>
+                <form className="form">
+                    <span>
+                        Yoshingiz:
+                    </span>    
+                    <input type="text" onChange={this.changeHandler} />
+                    <p>{this.state.age}</p>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default ClassComponentApp;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import ReactDOM from "react-dom/client";
 import {StrictMode} from "react";
-import App from "./App";
+// import App from "./App";
+import ClassComponentApp from "./ClassComponentApp";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <ClassComponentApp name="Sherzod" />
   </StrictMode>
 )
