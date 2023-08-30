d5df0436735eff2dfacc5d4336b92f63e41f0c30
Sherzod Aliyev <shortfilmuz@gmail.com>
2023-08-30 17:09:37 +0500

    change classComponent

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import ReactDOM from "react-dom/client";
 import {StrictMode} from "react";
-import App from "./App";
-// import ClassComponentApp from "./practise/ClassComponentApp";
+// import App from "./App";
+import ClassComponentApp from "./practise/ClassComponentApp";
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
-    {/* <ClassComponentApp /> */}
-    <App />
+    <ClassComponentApp />
+    {/* <App /> */}
   </StrictMode>
 )
--- src/practise/ClassComponentApp.js
+++ src/practise/ClassComponentApp.js
@@ -1,4 +1,5 @@
 import {Component} from "react";
+import "./style.css";
 
 class ClassComponentApp extends Component {
     constructor(props) {
@@ -6,6 +7,8 @@ class ClassComponentApp extends Component {
         this.state = {
             counter: 0,
             age: "",
+            isLogin: false,
+            isDisplay: true,
         }
     }
 
@@ -27,10 +30,34 @@ class ClassComponentApp extends Component {
     }
 
     changeHandler = (e) => {
-        // console.log(e.target.value);
         this.setState({
             age: e.target.value,
         })
+    }
+
+    onToggle = () => {
+        this.setState(prevState => ({isLogin: !prevState.isLogin}))
+    }
+
+    clickDisplay = () => {
+        this.setState({
+            isDisplay: false,
+        })
+    }
+
+
+    componentDidMount(){
+        console.log("Mount");
+        document.title = `Count: ${this.state.counter}`
+    }
+
+    componentDidUpdate(){
+        console.log("Update");
+        document.title = `Count: ${this.state.counter}`
+    }
+
+    componentWillUnmount() {
+        console.log("Unmount");
     }
 
     render() {
@@ -38,23 +65,30 @@ class ClassComponentApp extends Component {
         const {name} = this.props;
         const {counter} = this.state;
         return (
-            <div>
+            <>
+            <div className="counter-box">
                 <h1>{name}</h1>
                 <div className="counter">
-                    <button onClick={() => this.changeCounter("add")}>Add + 1</button>
                     <h2>{`Counter: ${counter}`}</h2>
+                    <button onClick={() => this.changeCounter("add")}>Add + 1</button>
                     <button onClick={() => this.changeCounter("delete")}>Delete - 1</button>
-                    <br /><br />
+                    <br />
                     <button onClick={() => this.changeCounter("reset")}>Reset</button>
                 </div>
                 <form className="form">
-                    <span>
+                    <label htmlFor="age">
                         Yoshingiz:
-                    </span>    
-                    <input type="text" onChange={this.changeHandler} />
+                    </label>    
+                    <input id="age" type="text" onChange={this.changeHandler} />
                     <p>{this.state.age}</p>
                 </form>
+                <div className="toggleIsLogin">
+                   {this.state.isLogin ? <p>Login</p> : null}
+                   <button onClick={this.onToggle}>Toggle</button>
+                </div>
             </div>
+            <button onClick={this.clickDisplay}>Click Display</button>
+            </>
         )
     }
 }
